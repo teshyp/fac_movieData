73bc4e6b1ec4d5379b8fa120a904c1a810f6ad55
Tesh Patel <tesh.p@hotmail.co.uk>
2022-10-12 22:01:35 +0100

    practicing object/array methods and loops. Next step: render returned data to dom

--- app.js
+++ app.js
@@ -34,7 +34,37 @@ let movieData = {
   },
 };
 
+const galleryContainer = document.getElementById("movie-gallery-container");
+const movieTile = document.getElementsByClassName("movie-tile");
+// const moviePosters = document.getElementsByClassName("movie-poster");
+// const movieTitle = document.getElementsByClassName("movie-title");
+// const moviePlot = document.getElementsByClassName("movie-plot");
+// const castContainer = document.getElementsByClassName("cast-div");
+// const castList = document.getElementsByClassName("class-list");
+// const castMember = document.getElementsByClassName("cast-member");
+// const runtime = document.getElementsByClassName("runtime");
+// const rating = document.getElementsByClassName("rating");
+
 // add option to filter movies by rating or/and year
 // allow user to add own movie
 
 // loop through each prop and send to screen
+
+function renderMovieData(movieData) {
+  const movieKeys = Object.keys(movieData);
+  const movieInfo = Object.entries(movieData);
+
+  // for each key return values
+
+  for (let i = 0; i < movieKeys.length; i++) {
+    // step 1. return movie keys
+    console.log(movieKeys[i]);
+    // step 2. get key/value for movie info
+    for (const [key, value] of Object.entries(movieInfo[1][1])) {
+      console.log(key, value);
+    }
+
+    // step 3. render to dom - if there is no rating/year/runtime key then leave blank or put place holder in dom
+  }
+}
+renderMovieData(movieData);
